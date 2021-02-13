d042263f33163a5bc538c1b2502faa5a93f131f0
LougansJs <lougans.lougans99@gmail.com>
2021-02-12 23:59:18 -0300

    :rocket: Atualizando views e criando novas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  root to: 'home#index'
+  # root to: 'home#index'
+  resource :home
+  root to: 'sites#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
